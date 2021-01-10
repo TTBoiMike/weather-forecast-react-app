63c3d8312e526ac460155fce94cdbcc978a9805e
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-10 20:12:42 +0000

    adds icons and header image

--- src/components/App.js
+++ src/components/App.js
@@ -29,6 +29,14 @@ class App extends React.Component {
 
   // 02ef4d3d345333c1a5f6f1a75f10207c
 
+  determineDayTime(code) {
+    if (code.includes("n")) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   updateLocation = (input, areaCode) => {
     let city = input.toLowerCase();
     // call fecth to api to retrieve city information
@@ -56,8 +64,10 @@ class App extends React.Component {
           currentWeather: {
             temperature: Math.round(json.current.temp),
             windSpeed: Math.round(json.current.wind_speed),
-            summary: json.current.weather[0].description
+            summary: json.current.weather[0].description,
+            isDayTime: this.determineDayTime(json.current.weather[0].icon)
           },
+          iconDisplay: json.current.weather[0].icon,
           forecast: json.daily
         })
       })
@@ -65,6 +75,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <Search updatelocation={this.updateLocation}/>
--- src/components/location.js
+++ src/components/location.js
@@ -2,40 +2,98 @@ import React from 'react'
 import Forecast from './forecast'
 import { Accordion, Card, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../App.css'
-//icon imports
+import '../App.css';
+import D1 from '../assets/icons/01d.png'
+import N1 from '../assets/icons/01n.png'
+import D2 from '../assets/icons/02d.png'
+import N2 from '../assets/icons/02n.png'
+import D3 from '../assets/icons/03d.png'
+import N3 from '../assets/icons/03n.png'
+import D4 from '../assets/icons/04d.png'
+import N4 from '../assets/icons/04n.png'
+import D9 from '../assets/icons/09d.png'
+import N9 from '../assets/icons/09n.png'
+import D10 from '../assets/icons/10d.png'
+import N10 from '../assets/icons/10n.png'
+import D11 from '../assets/icons/11d.png'
+import N11 from '../assets/icons/11n.png'
+import D13 from '../assets/icons/13d.png'
+import N13 from '../assets/icons/13n.png'
+import D50 from '../assets/icons/50d.png'
+import N50 from '../assets/icons/50n.png'
+import Daytime from '../assets/daytime-header.jpg'
+import Nighttime from '../assets/night-sky-header.jpg'
 
 class Location extends React.Component {
 
-    // displayIcon(phrase, time) {
-    //     console.log(phrase, time)
-    //     if(time) {
-    //         console.log("day")
-    //     } else {
-    //         console.log("night")
-    //     }
-    // }
+    displayIcon(code, time) {
+        console.log(code, time)
+        if(time) {
+            switch(code) {
+                case "01d":
+                    return D1
+                case "02d":
+                    return D2
+                case "03d":
+                    return D3
+                case "04d":
+                    return D4
+                case "09d":
+                    return D9
+                case "10d":
+                    return D10
+                case "11d":
+                    return D11
+                case "13d":
+                    return D13
+                case "50d":
+                    return D50
+            }
+            console.log("day")
+        } else {
+            switch (code) {
+                case "01n":
+                    return N1
+                case "02n":
+                    return N2
+                case "03n":
+                    return N3
+                case "04n":
+                    return N4
+                case "09n":
+                    return N9
+                case "10n":
+                    return N10
+                case "11n":
+                    return N11
+                case "13n":
+                    return N13
+                case "50n":
+                    return N50
+            }
+        }
+    }
 
     render() {
         return (
             <div style={this.props.appInformation.location === "" ? {display: "none"} : {display: "block"}} className="location">
-                <div className="banner"></div>
+                <div className="banner" style={{backgroundImage: this.props.appInformation.currentWeather.isDayTime ? `url(${Daytime})` : `url(${Nighttime})`}}></div>
                 <div className="location-current-conditions">
                     <div className="current-weather-icon">
-                        {/* <img src={this.displayIcon(this.props.appInformation.currentWeather.summary, this.props.appInformation.currentWeather.isDayTime)}></img> */}
+                        <img src={this.displayIcon(this.props.appInformation.iconDisplay, this.props.appInformation.currentWeather.isDayTime)}></img>
                     </div>
                     <h4>{this.props.appInformation.location}</h4>
                     <p>{this.props.appInformation.currentWeather.summary}</p>
                     <div>
-                    <p className="temp">{Math.floor(this.props.appInformation.currentWeather.temperature)}<sup> o</sup>C</p>
-                    <p>{this.props.appInformation.currentWeather.windSpeed} mph</p>
+                        <h1 className="temp">{Math.floor(this.props.appInformation.currentWeather.temperature)}<sup> o</sup>C</h1>
+                        <p>Wind speed: {this.props.appInformation.currentWeather.windSpeed} mph</p>
                     </div>
                 </div>
                 <Accordion>
                     <Card>
                         <Card.Header>
                         <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            <p><strong>5 Day Weather Forecast</strong></p>
+                            <p><strong>View Weather Forecast</strong></p>
                         </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
