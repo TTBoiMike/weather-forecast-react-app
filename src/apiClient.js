d9d8eed82f78e6e9427f8de64d6c59bad0b382eb
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-13 18:01:35 +0000

    refactor apiclient

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,1 +1,30 @@
+export class ApiClient {
 
+    // error handling
+    status(response) {
+        if(response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response)
+        } else {
+            return Promise.reject(new Error(response.statusText))
+        }
+    }
+
+    getLocation(input, areaCode) {
+        let city = input.toLowerCase();
+        return this.getRequest(`https://api.openweathermap.org/data/2.5/weather?q=${city},${areaCode}&appid=02ef4d3d345333c1a5f6f1a75f10207c&units=metric`)
+    }
+
+    getForecast(long, lat) {
+        console.log("fetch forecast")
+        return this.getRequest(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,alerts,hourly&appid=02ef4d3d345333c1a5f6f1a75f10207c&units=metric`)
+    }
+
+    getRequest(apiURL) {
+        return fetch(apiURL)
+        .then(this.status)
+        .catch(err => {
+            console.log(err)
+            alert(`Sorry, we couldn't find forecast information.`)
+        })
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import Search from './search'
 import Location from './location'
-import '../App.css';
+import {ApiClient} from '../apiClient'
+import '../App.css'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       location: "",
-      coordinates: {
-        lon: "",
-        lat: ""
-      },
       currentWeather: {
         temperature: "",
         windSpeed: "",
@@ -21,6 +18,7 @@ class App extends React.Component {
       iconDisplay: undefined,
       forecast: []
     }
+    this.ApiClient = new ApiClient()
   }
 
   clearApp = () => {
@@ -49,39 +47,31 @@ class App extends React.Component {
     }
   }
 
-  updateLocation = (input, areaCode) => {
-    let city = input.toLowerCase();
-    // call fecth to api to retrieve city information
-    let locationApiResponse = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${areaCode}&appid=02ef4d3d345333c1a5f6f1a75f10207c&units=metric`)
-    locationApiResponse.then((data) => {
-      data.json().then((json) => {
-        console.log("json", json)
-        this.updateForecast(json.name, json.coord.lon, json.coord.lat)
-      }).catch((err) => {
-        console.log(err)
-        alert(`Sorry, we couldn't find any information for: ${input}, ${areaCode}.`)
+  fetchAppData = (input, areaCode) => {
+    this.ApiClient.getLocation(input, areaCode)
+    .then(data => data.json())
+    .then(json => {
+      console.log(json)
+      this.setState({
+        location: json.name
       })
-    })
-  }
-
-  // // update the forecast for the location using the location coordinates
-  updateForecast(location, long, lat) {
-    let forecastApiResponse = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,alerts,hourly&appid=02ef4d3d345333c1a5f6f1a75f10207c&units=metric`)
-    forecastApiResponse.then((data) => {
-      data.json().then((json) => {
-        this.setState({
-          location: location,
-          coordinates: json.coord,
-          currentWeather: {
-            temperature: Math.round(json.current.temp),
-            windSpeed: Math.round(json.current.wind_speed),
-            summary: json.current.weather[0].description,
-            isDayTime: this.determineDayTime(json.current.weather[0].icon)
-          },
-          iconDisplay: json.current.weather[0].icon,
-          forecast: json.daily.slice(1, 8)
+      this.ApiClient.getForecast(json.coord.lon, json.coord.lat)
+        .then(data => data.json())
+        .then(json => {
+            this.setState({
+              currentWeather: {
+                temperature: Math.round(json.current.temp),
+                windSpeed: Math.round(json.current.wind_speed),
+                summary: json.current.weather[0].description,
+                  isDayTime: this.determineDayTime(json.current.weather[0].icon)
+              },
+              iconDisplay: json.current.weather[0].icon,
+              forecast: json.daily.slice(1, 8)
+            })
         })
-      })
+    })
+    .catch((err) => {
+      console.log(err)
     })
   }
 
@@ -89,7 +79,7 @@ class App extends React.Component {
     console.log(this.state)
     return (
       <div className="App">
-        <Search updatelocation={this.updateLocation}/>
+        <Search updatelocation={this.fetchAppData}/>
         <Location appInformation={this.state} clearapp={this.clearApp}/>
       </div>
     )
